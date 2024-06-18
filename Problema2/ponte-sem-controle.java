d91b5e80f98f1753802dc59f1c52e18c468cffc8
João Marrocos <135621408+jlsm2@users.noreply.github.com>
2024-06-18 11:22:38 -0300

    Create ponte-sem-controle.java

---
+++ Problema2/ponte-sem-controle.java
@@ -0,0 +1,28 @@
+class Carro implements Runnable {
+    private String direcao;
+    private int id;
+
+    Carro(String direcao, int id) {
+        this.direcao = direcao;
+        this.id = id;
+    }
+
+    public void run() {
+        try {
+            System.out.println("Carro " + id + " vindo da " + direcao + " entrou na ponte.");
+            Thread.sleep(1000);
+            System.out.println("Carro " + id + " vindo da " + direcao + " saiu da ponte.");
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
+
+public class Threads {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            String direcao = (i % 2 == 0) ? "esquerda" : "direita";
+            new Thread(new Carro(direcao, i)).start();
+        }
+    }
+} 
