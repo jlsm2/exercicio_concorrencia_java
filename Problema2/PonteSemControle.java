bf773474c22ec53e9f2847fd11ad7c190dc95a1c
João Marrocos <135621408+jlsm2@users.noreply.github.com>
2024-06-21 14:19:51 -0300

    Update PonteSemControle.java

--- Problema2/PonteSemControle.java
+++ Problema2/PonteSemControle.java
@@ -1,6 +1,7 @@
 class Carro implements Runnable {
     private String direcao;
     private int id;
+    private static boolean ponteOcupada = false;
 
     Carro(String direcao, int id) {
         this.direcao = direcao;
@@ -9,11 +10,26 @@ class Carro implements Runnable {
 
     public void run() {
         try {
+            atravessar();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private void atravessar() throws InterruptedException {
+        
+        Thread.sleep(1000);
+        
+        System.out.println("Carro " + id + " vindo da " + direcao + " quer entrar na ponte.");
+        
+        if (!ponteOcupada) {
+            ponteOcupada = true;
             System.out.println("Carro " + id + " vindo da " + direcao + " entrou na ponte.");
             Thread.sleep(1000);
             System.out.println("Carro " + id + " vindo da " + direcao + " saiu da ponte.");
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
+            ponteOcupada = false;
+        } else {
+            System.out.println("Carro " + id + " vindo da " + direcao + " não conseguiu atravessar.");
         }
     }
 }
@@ -25,4 +41,4 @@ public class PonteSemControle {
             new Thread(new Carro(direcao, i)).start();
         }
     }
-} 
+}
