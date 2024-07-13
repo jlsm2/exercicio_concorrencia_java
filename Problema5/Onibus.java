c586bcd376c21d6c0f0b6521ed1ef68206c29397
Thomaz Cabral <127772709+thomazcabral@users.noreply.github.com>
2024-07-13 15:50:08 -0300

    código parando
    pela interpretação da questão, talvez seja necessário fazer com que o código rode pra sempre mesmo quando não tiver ninguém na parada, mas é bom confirmar para fazer essa alteração, que deve ser bem tranquilo de ser feito

--- Problema5/Onibus.java
+++ Problema5/Onibus.java
@@ -8,25 +8,10 @@ não tem fila, todos tentam entrar ao mesmo tempo
 
 */
 
-/*
-
-o que faz o ThreadLocalRandom?
-por que volatile?
-código nunca para
-ônibus não sai assim que fica cheio
-mudar o output de quando o ônibus passa e não tem ninguém
-por que o join nas threads de ônibus?
-criar classe pra quando o ônibus sair
-tirar esse runnable
-entender esse tempo de chegada dos passageiros
-ver se dá pra tirar o String[] args da main
-ver se a main depois realmente funciona
-
- */
-
-import java.util.concurrent.locks.ReentrantLock;
-import java.util.concurrent.Semaphore;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.locks.ReentrantLock; // para controlar o acesso à região crítica
+import java.util.concurrent.Semaphore; // para controlar a quantidade de passageiros que podem entrar no ônibus
+import java.util.concurrent.CountDownLatch; // para contar quantos passageiros ainda faltam
+import java.util.concurrent.ThreadLocalRandom; // quando várias threads precisam gerar números aleatórios
 
 public class Onibus {
     private static final int CAPACIDADE_MAX = 50; // cabem apenas 50 pessoas nos ônibus
@@ -34,7 +19,11 @@ public class Onibus {
     private final Semaphore lugarOnibus = new Semaphore(0); // sem fair=true para não criar fila
     private final Semaphore entrarOnibus = new Semaphore(0); // sem fair=true para não criar fila
     private int passageirosEsperando = 0;
-    private boolean passageirosFim = false;
+    private final CountDownLatch latch; // poderia ser um booleano, mas precisaria usar volatile para que as threads vissem a mudança
+
+    public Onibus(int totalPassageiros) { // inicializando variáveis que dependem do número total de passageiros
+        this.latch = new CountDownLatch(totalPassageiros);
+    }
 
     public void chegadaPassageiro() {
         lock.lock(); // entrando na região crítica
@@ -46,13 +35,13 @@ public class Onibus {
         }
     }
 
-    public void chegadaOnibus() throws InterruptedException{ // throws necessário por causa do .acquire()
+    public void chegadaOnibus() {
         lock.lock(); // entrando na região crítica
         try {
             System.out.println("Ônibus chegou na parada");
             int passageirosNoOnibus = Math.min(passageirosEsperando, CAPACIDADE_MAX); // passageiros que vão embarcar no ônibus que chegou
             if (passageirosNoOnibus > 0) {
-                System.out.println(passageirosNoOnibus + "passageiros estão liberados para entrar no ônibus");
+                System.out.println(passageirosNoOnibus + " passageiros estão liberados para entrar no ônibus");
                 lugarOnibus.release(passageirosNoOnibus); // liberando os lugares do ônibus para os passageiros entrarem
             } else {
                 System.out.println("Ninguém está esperando");
@@ -60,16 +49,14 @@ public class Onibus {
         } finally {
             lock.unlock(); // saindo da região crítica
         }
+    }
 
-
+    public void saidaOnibus() throws InterruptedException { // throws necessário por causa do .acquire()
         entrarOnibus.acquire(Math.min(passageirosEsperando, CAPACIDADE_MAX));
         lock.lock(); // entrando na região crítica
         try {
             passageirosEsperando -= Math.min(passageirosEsperando, CAPACIDADE_MAX);
             System.out.println("Ônibus saindo. Passageiros restantes: " + passageirosEsperando);
-            if (passageirosEsperando == 0) {
-                passageirosFim = true; // a fim de encerrar o código quando não tiver mais ninguém
-            }
         } finally {
             lock.unlock(); // saindo da região crítica
         }
@@ -78,18 +65,13 @@ public class Onibus {
     public void entrarOnibus() throws InterruptedException { // throws necessário por causa do .sleep()
         lugarOnibus.acquire(); // tentando ocupar um lugar no ônibus
         System.out.println(Thread.currentThread().getName() + " está entrando no ônibus");
-        Thread.sleep(100); // simulando tempo para entrar no ônibus
         entrarOnibus.release();
-    }
-
-    public boolean fim() {
-        return passageirosFim;
     }
 
     public void criarOnibus() {
         Thread threadOnibus = new Thread(() -> { // criando a thread pro ônibus
             try {
-                while(!fim()) { // só cria o ônibus se tiver passageiro esperando
+                while (latch.getCount() > 0) { // enquanto ainda tiver passageiros pra entrar
                     Thread.sleep(ThreadLocalRandom.current().nextInt(1000, 3000)); // ônibus passam a cada 1 a 3 segundos
                     chegadaOnibus();
                 }
@@ -99,19 +81,21 @@ public class Onibus {
         });
 
         threadOnibus.start(); // começando a thread
-        try {
-            threadOnibus.join(); // não sei por que
-        } catch (InterruptedException e) { // precisa por causa do .join()
-            Thread.currentThread().interrupt();
-        }
     }
 
-    class Passageiro implements Runnable {
-        public void run() {
+    class Passageiro {
+        public void executarPassageiro() {
             try {
                 Thread.sleep(ThreadLocalRandom.current().nextInt(1000, 5000)); // simulando o tempo de chegada dos passageiros
                 chegadaPassageiro();
                 entrarOnibus();
+
+                lock.lock(); // entrando na região crítica
+                try {
+                    latch.countDown(); // decrementando o latch
+                } finally {
+                    lock.unlock(); // saindo da região crítica
+                }
             } catch (InterruptedException e) { // precisa por causa do .sleep()
                 Thread.currentThread().interrupt();
             }
@@ -120,13 +104,15 @@ public class Onibus {
 
     public static void main(String[] args) {
         final int quantidadePassageiros = 100; // número fixo pela monitoria
-        Onibus parada = new Onibus();
+        Onibus parada = new Onibus(quantidadePassageiros);
 
-        for(int i = 0; i < quantidadePassageiros; i++) {
-            new Thread(parada.new Passageiro()).start(); // criando passageiros
+        for (int i = 0; i < quantidadePassageiros; i++) {
+            final Passageiro passageiro = parada.new Passageiro();
+            Thread passageiroThread = new Thread(() -> passageiro.executarPassageiro());
+            passageiroThread.setName("Passageiro " + (i+1));
+            passageiroThread.start();
         }
 
         parada.criarOnibus(); // gerando os ônibus e colocando o sistema pra funcionar
-        System.out.println("FIM"); // todos os passageiros sendo atendidos
     }
 }
